4deb3f67c9d44e75392e3b0bc3ef5bcface2940d
Dasha <93427597+DashaAga@users.noreply.github.com>
2023-04-01 10:50:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,9 +5,7 @@ import numpy as np
 chat_id = 588908837 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    sqrt = 0
     sum = 0
     for i in np.nditer(x):
-      sqrt += i**2
       sum += i
-    return sqrt/len(x) - (sum/len(x))**2
+    return sum/len(x)*50
