553daccc2d0e64aabffc6115aed00dd9b54e5be7
Dasha <93427597+DashaAga@users.noreply.github.com>
2023-04-01 20:49:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,4 @@ import numpy as np
 chat_id = 588908837 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    sum = 0
-    for i in np.nditer(x):
-      sum += i
-    return sum/len(x)*50
+    return x.mean()
